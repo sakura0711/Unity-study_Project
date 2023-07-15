8d2a48adc0a5539538ba7dd9db7bb195fa83bdd4
sakura0711 <testuseroozx@gmail.com>
2023-07-15 15:45:36 +0800

    添加 player 動畫
    添加 player 移動時的動畫 & 待機動畫

--- 小紅帽 RPG/Assets/Script/player.cs
+++ 小紅帽 RPG/Assets/Script/player.cs
@@ -7,11 +7,13 @@ public class player : MonoBehaviour
     public float moveSpeed;
     public string playerName;
     SpriteRenderer playerFilpX;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         playerFilpX = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
     // Update is called once per frame
     void Update()
@@ -32,12 +34,18 @@ public class player : MonoBehaviour
         else if (Input.GetKey(KeyCode.A))
         {
             playerFilpX.flipX = false;
+            anim.SetBool("isMove", true);
             transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             playerFilpX.flipX = true;
+            anim.SetBool("isMove", true);
             transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
+        }
+        else
+        {
+            anim.SetBool("isMove", false);
         }
     }
 }
