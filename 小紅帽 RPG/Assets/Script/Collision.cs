f007ea0457a538082f760f0be983a434009a035a
sakura0711 <testuseroozx@gmail.com>
2023-07-19 22:22:28 +0800

    新增小紅帽外婆
    添加新 NPC

--- 小紅帽 RPG/Assets/Script/Collision.cs
+++ 小紅帽 RPG/Assets/Script/Collision.cs
@@ -10,28 +10,12 @@ public class Collision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        tipText.gameObject.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject.CompareTag("triggerHome"))
-        {
-            tipText.gameObject.SetActive(true);
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.CompareTag("triggerHome"))
-        {
-            tipText.gameObject.SetActive(false);
-        }
     }
 }
--- 小紅帽 RPG/Assets/Script/playerCollision.cs
+++ 小紅帽 RPG/Assets/Script/playerCollision.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+
 public class playerCollision : MonoBehaviour
 {
     bool isCollision = false;
@@ -12,7 +13,7 @@ public class playerCollision : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        tipText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -36,33 +37,61 @@ public class playerCollision : MonoBehaviour
                     tipText.gameObject.SetActive(false);
                     SceneManager.LoadScene(collider.gameObject.GetComponent<SceneInfo>().SceneName);
                 }
+                else if (collider.name == "grandmother")
+                {
+                    Debug.Log("小紅帽 碰撞到了 grandmother(奶奶)");
+
+                }
             }
         }
     }
 
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-
-        if (other.gameObject.name == "HomeTrigger" || other.gameObject.name == "doorTrigger")
+        if (other.gameObject.CompareTag("EventTrigger"))
+        {
+            tipText.gameObject.SetActive(true);
+        }
+        if (other.gameObject.name == "HomeTrigger" ||
+           other.gameObject.name == "doorTrigger" ||
+           other.gameObject.name == "grandmother")
         {
             collider = other;
             isCollision = true;
         }
-        // {
-        //     Debug.Log("小紅帽 碰撞到了 door(進門)");
-        //     gameObject.transform.position = new Vector3(0, -3, 0);
-
-        //     SceneManager.LoadScene(other.gameObject.GetComponent<SceneInfo>().SceneName);
-        // }
-        // if (other.gameObject.name == "doorTrigger")
-        // {
-        //     Debug.Log("小紅帽 碰撞到了 door(出門)");
-        //     gameObject.transform.position = new Vector3(21.77f, 22.6f, 0);
-        //     SceneManager.LoadScene(other.gameObject.GetComponent<SceneInfo>().SceneName);
-        // }
     }
+
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.gameObject.name == "HomeTrigger" || other.gameObject.name == "doorTrigger") { isCollision = false; collider = null; }
+        if (other.gameObject.CompareTag("EventTrigger"))
+        {
+            tipText.gameObject.SetActive(false);
+        }
+        {
+            if (other.gameObject.name == "HomeTrigger" ||
+                other.gameObject.name == "doorTrigger" ||
+                other.gameObject.name == "grandmother")
+            {
+                isCollision = false;
+                collider = null;
+            }
+        }
+        playerMoveFun(1, "strinh");
+    }
+
+    /// <summary>
+    /// tedstste
+    /// </summary>
+    /// 
+    /// <param name="a">awdawd</param>
+    /// <param name="stringTalk">awdadw</param>
+    /// <returns>ddd</returns>
+    /// <code>dawdij</code>
+    public int playerMoveFun(int a, string stringTalk)
+    {
+        a = 0;
+        stringTalk = "dd";
+        return 0;
     }
 }
